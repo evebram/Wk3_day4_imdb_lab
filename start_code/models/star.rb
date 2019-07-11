f466ae5432756d9b9702da00784446e819bb7eaa
= <evebramley@me.com>
2019-07-11 14:19:05 +0100

    created tables

--- start_code/models/star.rb
+++ start_code/models/star.rb
@@ -9,4 +9,10 @@ class Star
     @last_name = options['last_name']
   end
 
+  def save()
+    sql = "INSERT INTO "
+    values = []
+    @id = SqlRunner.run(sql, values).first['id'].to_i
+  end
+
 end
